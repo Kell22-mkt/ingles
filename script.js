a4ecb5b6eafe632ed437fd6c097759940019aff1
Kell22-mkt <kellyckaroline@gmail.com>
2022-07-12 19:33:54 -0300

    ingles

---
+++ script.js
@@ -0,0 +1,17 @@
+let comedia = [{
+    title: 'Vovozona',
+    diretor: 'Raja Gosnell',
+    year: 2000
+}]
+
+let action = [{
+    title: 'Fora do mapa',
+    diretor:'Ruben Fleischer',
+    year:2022
+}]
+
+let fantasy = [{
+    title: 'Senhor dos aneis',
+    diretor: 'Peter Jackson',
+    year: 2002
+}] 
